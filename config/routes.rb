b42711e2f65aed75e055edd370c96b1b2fbaacee
sedx876 <swatkins876@gmail.com>
2020-11-08 11:01:19 -0500

    added elements scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :authors
   get 'home/index'
   root to: "home#index"
+  scope module: 'authors' do
+    resources :posts do
+      resources :elements
+    end
+  end
 end
